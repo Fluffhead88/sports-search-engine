5a667aed28e609af1bae8257949ae7828003f9a4
Fluffhead88 <thigpen.zach@gmail.com>
2018-05-29 21:59:13 -0400

    table populating and menu working

--- sports_search_engine/normal.py
+++ sports_search_engine/normal.py
@@ -4,29 +4,28 @@ import psycopg2
 
 db = records.Database("postgres://localhost/clemson_stats_db")
 
+db.query("DROP TABLE clemson_stats_db");
 
 db.query("CREATE TABLE clemson_stats_db (rk NUMERIC(2), player VARCHAR(30), rush_att NUMERIC(3), rush_yds NUMERIC(3), rush_avg DECIMAL, rush_td NUMERIC(2))");
 
 rk = [1, 2, 3, 4, 5]
 player = ["Kelly Bryant", "Travis Etienne", "Tavien Feaster", "Adam Choice", "CJ Fuller"]
-rush_att = [192, 107, 107,67, 58]
+rush_att = [192, 107, 107, 67, 58]
 rush_yds = [665, 766, 669, 326, 217]
 rush_avg = [3.5, 7.2, 6.3, 4.9, 3.7]
 rush_td = [11, 13, 7, 6, 3]
 
-db.query("INSERT INTO clemson_stats_db VALUES (:rk, :player, :rush_att, :rush_yds, :rush_avg, :rush_td,);")
+db.query("INSERT INTO clemson_stats_db VALUES (:rk, :player, :rush_att, :rush_yds, :rush_avg, :rush_td);",
+ rk = 1, player = "Kelly Bryant", rush_att = 192, rush_yds = 665, rush_avg = 3.5, rush_td = 11)
 
-for rank in rk:
-    db.query.append(rk)
-for player in player:
-    db.query.append(player)
-for rush_att in rush_att:
-    db.query.append(rush_att)
-for rush_yds in rush_yds:
-    db.query.append(rush_yds)
-for rush_avg in rush_avg:
-    db.query.append(rush_avg)
-for rush_td in rush_td:
-    db.query.append(rush_td)
+db.query("INSERT INTO clemson_stats_db VALUES (:rk, :player, :rush_att, :rush_yds, :rush_avg, :rush_td);",
+ rk = 2, player = "Travis Etienne", rush_att = 107, rush_yds = 766, rush_avg = 7.2, rush_td = 13)
 
-"DROP TABLE clemson_stats_db";
+db.query("INSERT INTO clemson_stats_db VALUES (:rk, :player, :rush_att, :rush_yds, :rush_avg, :rush_td);",
+ rk = 3, player = "Tavien Feaster", rush_att = 107, rush_yds = 669, rush_avg = 6.3, rush_td = 7)
+
+db.query("INSERT INTO clemson_stats_db VALUES (:rk, :player, :rush_att, :rush_yds, :rush_avg, :rush_td);",
+ rk = 4, player = "Adam Choice", rush_att = 67, rush_yds = 326, rush_avg = 4.9, rush_td = 6)
+
+db.query("INSERT INTO clemson_stats_db VALUES (:rk, :player, :rush_att, :rush_yds, :rush_avg, :rush_td);",
+ rk = 5, player = "CJ Fuller", rush_att = 158, rush_yds = 217, rush_avg = 3.7, rush_td = 3)
--- sports_search_engine/normal_user.py
+++ sports_search_engine/normal_user.py
@@ -2,23 +2,38 @@ import records
 
 db = records.Database("postgres://localhost/clemson_stats_db")
 
-player_input = input("Player? ")
 
-rows = db.query("SELECT * FROM clemson_stats_db WHERE player = :data ;", data = player_input)
 
-for row in rows:
-    print(row.player)
-
-new_rank = input("New Rank? ")
-new_player = input("New Player? ")
-new_rush_att = input("New Rushing Attempts? ")
-new_rush_yds = input("New Rushing Yards? ")
-new_rush_avg = input("New Rushing Average? ")
-
-db.query("INSERT INTO clemson_stats_db VALUES (:rk, :player, :rush_att, :rush_yds, :rush_avg);",
-        rk = new_rank,
-        player = new_player,
-        rush_att = new_rush_att,
-        rush_yds = new_rush_yds,
-        rush_avg = new_rush_avg
-        )
+choice = True
+while choice:
+    print("""
+    Clemson Rushing Statistics for 2017 Football Season
+    1.Search by Player
+    2.Search by Rank
+    3.Input New Player Stats
+    4.Exit/Quit
+    """)
+    choice = input("Choose a number > ")
+    if choice == "1":
+      player_input = input("Player? ")
+      rows = db.query("SELECT * FROM clemson_stats_db WHERE player = :data ;", data = player_input)
+      for row in rows:
+          print(row.player)
+    elif choice == "2":
+        pass
+    elif choice == "3":
+        new_rank = input("New Rank? ")
+        new_player = input("New Player? ")
+        new_rush_att = input("New Rushing Attempts? ")
+        new_rush_yds = input("New Rushing Yards? ")
+        new_rush_avg = input("New Rushing Average? ")
+        db.query("INSERT INTO clemson_stats_db VALUES (:rk, :player, :rush_att, :rush_yds, :rush_avg);",
+                rk = new_rank,
+                player = new_player,
+                rush_att = new_rush_att,
+                rush_yds = new_rush_yds,
+                rush_avg = new_rush_avg
+                )
+    elif choice == "4":
+        print("\n Goodbye")
+        choice = False
