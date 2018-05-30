9b7ad6aaa0b0a5562d08b57fd81b4fa5329946ab
Fluffhead88 <thigpen.zach@gmail.com>
2018-05-29 22:36:13 -0400

    all functionality working but not printing from table

--- sports_search_engine/normal.py
+++ sports_search_engine/normal.py
@@ -8,12 +8,12 @@ db.query("DROP TABLE clemson_stats_db");
 
 db.query("CREATE TABLE clemson_stats_db (rk NUMERIC(2), player VARCHAR(30), rush_att NUMERIC(3), rush_yds NUMERIC(3), rush_avg DECIMAL, rush_td NUMERIC(2))");
 
-rk = [1, 2, 3, 4, 5]
+"""rk = [1, 2, 3, 4, 5]
 player = ["Kelly Bryant", "Travis Etienne", "Tavien Feaster", "Adam Choice", "CJ Fuller"]
 rush_att = [192, 107, 107, 67, 58]
 rush_yds = [665, 766, 669, 326, 217]
 rush_avg = [3.5, 7.2, 6.3, 4.9, 3.7]
-rush_td = [11, 13, 7, 6, 3]
+rush_td = [11, 13, 7, 6, 3]"""
 
 db.query("INSERT INTO clemson_stats_db VALUES (:rk, :player, :rush_att, :rush_yds, :rush_avg, :rush_td);",
  rk = 1, player = "Kelly Bryant", rush_att = 192, rush_yds = 665, rush_avg = 3.5, rush_td = 11)
--- sports_search_engine/normal_user.py
+++ sports_search_engine/normal_user.py
@@ -8,32 +8,40 @@ choice = True
 while choice:
     print("""
     Clemson Rushing Statistics for 2017 Football Season
-    1.Search by Player
-    2.Search by Rank
-    3.Input New Player Stats
-    4.Exit/Quit
+
+    1. Search by Player
+    2. Search by Rank
+    3. Input New Player Stats
+    4. Exit
     """)
     choice = input("Choose a number > ")
     if choice == "1":
       player_input = input("Player? ")
       rows = db.query("SELECT * FROM clemson_stats_db WHERE player = :data ;", data = player_input)
       for row in rows:
-          print(row.player)
+          print(rows)
     elif choice == "2":
-        pass
+      player_input = input("Rank? ")
+      rows = db.query("SELECT * FROM clemson_stats_db WHERE rk = :data ;", data = player_input)
+      for row in rows:
+          print(rows)
     elif choice == "3":
-        new_rank = input("New Rank? ")
-        new_player = input("New Player? ")
-        new_rush_att = input("New Rushing Attempts? ")
-        new_rush_yds = input("New Rushing Yards? ")
-        new_rush_avg = input("New Rushing Average? ")
-        db.query("INSERT INTO clemson_stats_db VALUES (:rk, :player, :rush_att, :rush_yds, :rush_avg);",
+        new_rank = input("Rank? > ")
+        new_player = input("Player? > ")
+        new_rush_att = input("Rushing Attempts? > ")
+        new_rush_yds = input("Rushing Yards? > ")
+        new_rush_avg = input("Rushing Average? > ")
+        new_rush_tds = input("Rushing TD's? > ")
+        rows = db.query("INSERT INTO clemson_stats_db VALUES (:rk, :player, :rush_att, :rush_yds, :rush_avg, :rush_tds);",
                 rk = new_rank,
                 player = new_player,
                 rush_att = new_rush_att,
                 rush_yds = new_rush_yds,
-                rush_avg = new_rush_avg
+                rush_avg = new_rush_avg,
+                rush_tds = new_rush_tds
                 )
+        for row in rows:
+            print(rows)
     elif choice == "4":
-        print("\n Goodbye")
+        print("\n   GO TIGERS!")
         choice = False
